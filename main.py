d99f32bc94224146846ede7a270e5a9d8d8d2a79
Edilma Riano <edilmaria@hotmail.com>
2019-10-03 15:10:38 -0400

    Templates for pages created.  Ready to start logic

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['DEBUG'] = True
 #TODO fix the database conexion
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:edilma@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:edilma@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'Lily'
@@ -13,18 +13,34 @@ app.secret_key = 'Lily'
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    content = db.Column(db.String(4294967295))
+    content = db.Column(db.Text)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self,title,content):
+    def __init__(self,title,content, owner):
         self.title = title
         self.content= content
+        self.owner= owner
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique = True, nullable=False)
+    password = db.Column(db.String(30))
+    posts = db.relationship('Post', backref='owner')
+
+    def __init__(self,username,password):
+        self.username = username
+        self.password= password
+        self.owner_id= owner_id
 
 
 
 @app.route('/', methods=["GET"])
 def index():
     return redirect("/blog")
+
+
+
+
 
 
 
