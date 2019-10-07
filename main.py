c3fc2c77db4dc92d536ffe72b8277bd1ba41ec85
Edilma Riano <edilmaria@hotmail.com>
2019-10-07 12:06:31 -0400

    create login and signup page.  Login system working

--- main.py
+++ main.py
@@ -5,20 +5,78 @@ from helpers import *
 
 
 
-#route login - when a user signup and exist send it to index
+#route login - when a user tries to log in: allow and send to new post or errors flashed, click in signup
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+    if request.method == 'GET':
+        return render_template('login.html')
     if request.method == 'POST':
-        email = request.form['email']
+        username = request.form['username']
         password = request.form['password']
-        user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(username=username).first()
         if user and user.password == password:
-            session['email'] = email
+            session['username'] = username
             flash("Logged in")
-            return redirect('/')
+            return redirect('/newpost')
         else:
             flash('User password incorrect, or user does not exist', 'error')
     return render_template('login.html')
+
+
+    
+
+        
+#Route signgup.  Request the user information and verify it using the verify
+#function that is located in the helpers.py file. 
+@app.route('/signup', methods=['POST', 'GET'])
+def register():
+    if request.method =='GET':
+        return render_template('signup.html' ,errors=["","","",""], infoUser=["","","",""])
+    if request.method == 'POST':
+        username= request.form["username"]
+        password = request.form["password"]
+        verifyPassword = request.form["verifyPassword"]
+        infoUser =[username,password,verifyPassword]
+        errors = validateUserinfo(infoUser)
+        if (errors[0] !='') or  (errors[1] !='') or  (errors[2] !='')  :
+            return render_template('signup.html',errors=errors, infoUser=infoUser)
+        else:
+            existing_user = User.query.filter_by(username=username).first()
+            if not existing_user:
+                new_user = User(username, password)
+                db.session.add(new_user)
+                db.session.commit()
+                session['username'] = username
+                return redirect('/login')
+            else:
+                flash( 'User already exist.  Please log in')
+                return redirect('/login')
+
+    return render_template('signup.html')
+
+
+#Logout route - session ends. 
+
+@app.route('/logout')
+def logout():
+    del session['username']
+    return redirect('/')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #route index - Show list of usernames
 
@@ -26,11 +84,8 @@ def login():
 @app.route('/', methods=['POST', 'GET'])
 def index():
     return redirect("/blog")
-    
 
-        
-#Route signgup.  Request the user information and verify it using the verify
-#function that is located in the helpers.py file. 
+
 
 
 
