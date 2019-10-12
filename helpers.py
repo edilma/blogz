11399d93ca3b7c61bbded35e5ec4632b754e2a62
Edilma Riano <edilmaria@hotmail.com>
2019-10-12 08:16:22 -0400

    new branch starting over almost.  Newpost not working

--- helpers.py
+++ helpers.py
@@ -1,5 +1,5 @@
 
-
+'''Function to validate the user information'''
 def validateUserinfo(infoUser):
     username= infoUser[0]
     password= infoUser[1]
@@ -33,47 +33,16 @@ def validateUserinfo(infoUser):
     return errors
 
 
-# def validateLogin(user_login, user):
-#     username= infoUser[0]
-#     password= infoUser[1]
-#     errors=["","",]
-#     existing_user = user
-#     if not existing_user:
-#         errors[0]='Username DO NOT exits.  Please enter a correct username '
-#     else:
-#         if not check_pw_hash(password,user.pw_hash):
-#             error[1] = 'Incorrect Password'
-#         else: 
-#             existing_user = [username,password]
-#             return existing_user
-#     return errors
-    
 
 
 
-
-    #Typographical errors in the fields
-    if (len(username)<=3) or (len(username)>=20) or (' ' in username):
-        errors[0]= 'Incorrect Username: Please use 3 or more letters and not spaces'
-
-    if (len(password)<=3) or (len(password)>=20) or (' ' in password):
-        errors[1]= 'Incorrect Password: Please use 3 or more letters and not spaces'
-        
-    # if (email.count('@')>1) or (email.count('.')>1):
-    #     errors[3]='Incorrect Password. Please write your password again.'
-    
-    # if (len(email)>0 and ( (len(email)<=3) or (len(email)>=20)  or (' ' in email))):
-    #     errors[3]='Incorrect email. Please write your email again.'
-
-    #Password and verification don't match
-    if password != verifyPassword:
-        errors[2] = 'We could not verify the password. Passwords do NOT match'
-
-    #error when fields are left  empty
-    if username=='':
-        errors[0]='Username can NOT be empty.  Please provide a username '
-    if password=='':
-        errors[1]='Password can NOT be empty.  Please provide a password '    
-    if verifyPassword=='':
-        errors[2]='Please write your password again.  We could not verify it'
-    return errors
+'''Function to validate the blog post'''  
+def validatePost(blog_name,blog_content):
+    title= blog_name
+    content = blog_content
+    if title=="":
+        return "Title is Required"
+    if content=="":
+        return "Content is required"
+    else:
+        return "ok"
--- main.py
+++ main.py
@@ -4,15 +4,43 @@ from app import app, db
 from helpers import *
 from hashutils import *
 
-
+'''Before request decide which routes are allowed to go and post new blogs'''
 @app.before_request
 def require_login():
     allowed_routes = ['login', 'viewPosts','register','index']
     if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect('/login')
 
+        
+'''Route signgup.  Request the user information and verify it using the verifyPassword
+function that is located in the helpers.py file. '''
+@app.route('/signup', methods=['POST', 'GET'])
+def register():
+    if request.method =='GET':
+        return render_template('signup.html' ,errors=["","","",""], infoUser=["","","",""])
+    if request.method == 'POST':
+        username= request.form["username"]
+        password = request.form["password"]
+        verifyPassword = request.form["verifyPassword"]
+        infoUser =[username,password,verifyPassword]
+        errors = validateUserinfo(infoUser)
+    if (errors[0] !='') or  (errors[1] !='') or  (errors[2] !='')  :
+        return render_template('signup.html',errors=errors, infoUser=infoUser)
+    else:
+        existing_user = User.query.filter_by(username=username).first()
+        if not existing_user:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return redirect('/newpost', session['username'])
+        else:
+            flash("The username {0} is already registered".format(username), 'danger')
+    return redirect('/login')
 
-#route login - when a user tries to log in: allow and send to new post or errors flashed, click in signup
+
+'''Route login. User that check ok are allowed and send to newpost with session. 
+If errors exist, they are flashed. If not username exit redirect to  signup'''
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'GET':
@@ -25,42 +53,31 @@ def login():
         if user and check_pw_hash(password,user.pw_hash):
             session['username'] = user.username
             flash("Logged in")
-            return redirect('/newpost', session['username'])
+            return redirect('/newpost',session['username'])
         else:
             return redirect ('/signup')
-            
-        
 
-        
-#Route signgup.  Request the user information and verify it using the verify
-#function that is located in the helpers.py file. 
-@app.route('/signup', methods=['POST', 'GET'])
-def register():
-    if request.method =='GET':
-        return render_template('signup.html' ,errors=["","","",""], infoUser=["","","",""])
+
+'''Route newpost requires the user to be logged in'''
+@app.route('/newpost', methods=['GET','POST'])
+def create():
+    owner = User.query.filter_by(username=session['username']).first()
     if request.method == 'POST':
-        username= request.form["username"]
-        password = request.form["password"]
-        verifyPassword = request.form["verifyPassword"]
-        infoUser =[username,password,verifyPassword]
-        errors = validateUserinfo(infoUser)
-        if (errors[0] !='') or  (errors[1] !='') or  (errors[2] !='')  :
-            return render_template('signup.html',errors=errors, infoUser=infoUser)
-        else:
-            existing_user = User.query.filter_by(username=username).first()
-            if not existing_user:
-                new_user = User(username, password)
-                db.session.add(new_user)
-                db.session.commit()
-                session['username'] = username
-                return redirect('/newpost', username=username)
-            else:
-                flash( 'User already exist.  Please log in')
-                return redirect('/login')
-
-    return render_template('signup.html')
+        blog_name= request.form['title']
+        blog_content = request.form['content']
+        isValid = validatePost (blog_name,blog_content)
+        if isValid=="ok":
+            new_post = Post(blog_name,blog_content,owner=user.id)
+            db.session.add(new_post)
+            db.session.commit() 
+        else: 
+            flash(isValid) 
+        posts = Post.query.filter_by(id=new_post.id).first()
+    return render_template('blog_post.html',posts=posts) 
 
 
+
+            
 #Logout route - session ends. 
 
 @app.route('/logout' , methods=['POST'])
@@ -78,71 +95,13 @@ def index():
     return render_template ('users.html', users=users)
 
 
-
-
-    
-
-
 @app.route('/blog', methods=["GET"])
 def viewPosts():
-    # if session['username']:
-    #     id =  request.args.get('id')
-    #     posts = Post.query.filter_by(id=id).all()
-    #     return render_template('/blog.html', posts=posts)
-    # else:
     posts = Post.query.all()
-    return render_template('/posts.html',posts=posts ) #,username=username)
+    return render_template('/posts.html',posts=posts )
 
 
-
-
-
-@app.route('/newpost', methods=["GET"])
-def create():
-    owner = User.query.filter_by(username =session['username']).first()
-    if owner in session:
-        return render_template('newpost.html', username = username)
-    else:
-        return redirect('/login')
-
-    # posts = Post.query.all()
-    # return render_template('posts.html', title=title, content=content)
-
-
-@app.route('/newpost', methods=['POST'])
-def GetContent():
-#     if request.method =='GET':
-#         username = session['username']
-#         return render_template('newpost.html', username=username)
     
-    if username in session:
-    #owner = User.query.filter_by(username=session['username']).first()
-        title= request.form ['title']
-        content = request.form ['content']
-
-    if not title:
-        flash ("Title can NOT be empty")
-        error="error"
-        return render_template('newpost.html', content=content)
-    if not content:
-        flash ("Content can NOT be empty")
-        return render_template('newpost.html',title=title)
-    #everything is ok, content, title and in session
-    else:
-        #owner = User.query.filter_by(username = session['username']).first()
-        new_post = Post(title,content, owner = user.id)
-        db.session.add(new_post)
-        db.session.commit()
-        #posts = Post.query.all()
-        #post_id = new_post.id 
-        posts = Post.query.filter_by(id=new_post.id).first()
-    
-        return render_template('blog_post.html',posts=posts)
-    
-
-
-
-
 
 if __name__ == "__main__":
     app.run()
